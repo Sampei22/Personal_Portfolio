cfd045336be0e7712e3a191ec64df2625ebd84be
Enrico Sanchini <e.sanchini93@gmail.com>
2025-09-07 19:25:53 +0200

    Delete assets/script.js
    Eliminato file script.js

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-function sendMail(){
-    let parms = {
-      nome : document.getElementById("name").value, 
-      cognome : document.getElementById("cognome").value,
-      descrizione : document.getElementById("descrizione").value,
-      email : document.getElementById("email").value,  
-    }
-  emailjs.send("service_oaq0vl4","templateid", parms).then(alert("Email inviata!!")) 
-}
