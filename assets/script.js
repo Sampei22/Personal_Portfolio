b7efe67e902bfa15c6ea86774c7b0635532d88dd
Enrico Sanchini <e.sanchini93@gmail.com>
2025-09-07 18:19:32 +0200

    Update script.js
    Inserito parametri script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
-
+function sendMail(){
+    let parms = {
+      nome : document.getElementById("name").value, 
+      cognome : document.getElementById("name").value,
+      email : document.getElementById("name").value,
+    }
+  emailjs.send("service_oaq0vl4","templateid", parms).then(alert("email inviata!!")) 
+}
