120148aaf29e5b0f41f5b63e8d2e61496ef857a4
Enrico Sanchini <e.sanchini93@gmail.com>
2025-09-09 12:01:39 +0200

    Update script.js
    Inserito if ed else

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ function sendMail(){
       descrizione : document.getElementById("descrizione").value,
       email : document.getElementById("email").value 
     }
-  emailjs.send("service_oaq0vl4","template_4wqhx6t", parms).then(alert("Email inviata!!")) 
+    if (document.getElementById("nome").value != "" && document.getElementById("email").value !=""){
+      emailjs.send("service_oaq0vl4","template_4wqhx6t", parms).then(alert("Grazie per averci contattato.")) }
+        else{
+            alert("Inserire i campi obbligatori") }
 }
+
 
